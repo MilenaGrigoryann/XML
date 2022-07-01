1b97b1aa49c88eb183d51707e8028992a2e512bf
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-07-01 16:16:06 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xml = XDocument.Load("people.xml");
+            var people = xml.Descendants("person");
+            var qualificationId = people.SelectMany(person => person.Elements("teacher"))
+                                        .SelectMany(teacher => teacher.Elements("qualifications"))
+                                        .SelectMany(qualifications => qualifications.Elements("qualification"))
+                                        .Select(qualification => qualification.Element("id").Value);
+            var uniqueQualificationId = qualificationId.Distinct();
+            foreach (var item in uniqueQualificationId)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
